555be6ecf8497312afe6bb67203f441e408300fb
smshinn <smshinn@csu.fullerton.edu>
2019-04-25 05:09:12 -0700

    source code added

---
+++ src/About_state.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <SDL.h>
+#include "State.h"
+
+/**
+ * State implementation for handling the about screen state
+ *
+ * See State class for documentation on init(), handle_events(), update(),
+ * render() and clean_up().
+ */
+class About_state : public State
+{
+public:
+	void init();
+	void handle_events(SDL_Event& event);
+	void update();
+	void render();
+	void clean_up();
+};
---
+++ src/Resource.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SDL.h>
+
+/**
+ * Abstract class for resources to inherit
+ */
+class Resource
+{
+public:
+	/**
+	 * Loads a resource file into memory
+	 *
+	 * @param file Name of a resource file
+	 *
+	 * @return True if a file was loaded successfully
+	 */
+	virtual bool load_from_file(char* file) = 0;
+};
