061017058d57a4c7f0d4e40b09281fb54350c672
smshinn <smshinn@csu.fullerton.edu>
2019-05-09 21:09:54 -0700

    re-upload files

---
+++ Tic-tac-toe/Theme.h
@@ -0,0 +1,14 @@
+/*
+ * Tic-tac-toe
+ * MIT Licence, Copyright 2017 Chris Kempson (chriskempson.com)
+ */
+
+#pragma once
+#include <SDL.h>
+#include "State.h"
+#include <fstream>
+using namespace std;
+
+void SetTheme(int number);
+
+int GetTheme();
---
+++ Tic-tac-toe/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * Tic-tac-toe
+ * MIT Licence, Copyright 2017 Chris Kempson (chriskempson.com)
+ */
+
+#include <iostream>
+#include <string>
+#include "Game.h"
+#include "Setting.h"
+#include "State_manager.h"
+#include "Resource_manager.h"
+#include "Title_state.h"
+#include "Theme.h"
+
+// App entry point
+int main(int argc, char* args[])
+{
+	SetTheme(1);
+	if (!game.init(Setting::game_name, Setting::window_width, Setting::window_height)) {
+		std::cout << "Application initialisation error. Exiting. " << std::endl;
+	}
+
+	State_manager::set_state(new Title_state());
+	Resource_manager::load_sound("Spy");
+	Resource_manager::get_sound("Spy")->play();
+
+	game.run();
+
+	Resource_manager::unload_sound("Spy");
+	return 0;
+}
