4e5dc38214e8a3cd0085ceedd70de410b1e20930
EALopez1228 <32082563+EALopez1228@users.noreply.github.com>
2019-05-15 22:41:15 -0700

    Adding documentation

--- Tic-tac-toe/Theme.h
+++ Tic-tac-toe/Theme.h
@@ -8,7 +8,8 @@
 #include "State.h"
 #include <fstream>
 using namespace std;
-
+//These are the declarations for the functions that will get the theme value
+// and set the theme value
 void SetTheme(int number);
 
 int GetTheme();
